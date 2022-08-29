1819a0e96b11ffa28c4306a235ec8c9c4f589908
Cherine <578153@student.belgiumcampus.ac.za>
2022-08-29 16:26:26 +0200

    Final Changes

--- src/main/java/Repositories/StudentRepository.java
+++ src/main/java/Repositories/StudentRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import Model.Student;
 
-public interface StudentRepository extends JpaRepository <Student, Long> {
-    
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
 }
--- src/main/java/project/registerloginadmin/RegisterloginadminApplication.java
+++ src/main/java/project/registerloginadmin/RegisterloginadminApplication.java
@@ -9,34 +9,34 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import Model.Student;
 import Repositories.StudentRepository;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) 
+@SpringBootApplication
 
-public class RegisterloginadminApplication implements CommandLineRunner{
+public class RegisterloginadminApplication /*implements CommandLineRunner*/{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterloginadminApplication.class, args);
 		
 	}
 
-	@Autowired
+	/*@Autowired
 	private StudentRepository studentRepository;
 	@Override
 	public void run(String... args) throws Exception {
 
-		/*Student student1=new Student("Cherine", "Wam", "cherine@email.com");
+		Student student1=new Student("Cherine", "Wam", "cherine@email.com");
 		studentRepository.save(student1);
 
 		Student student2=new Student("Julian", "Bach", "julian@email.com");
 		studentRepository.save(student2);
 
 		Student student3=new Student("Johan", "Toit", "johan@email.com");
-		studentRepository.save(student3);*/
+		studentRepository.save(student3);
 
 
 
 	
 		
-	}
+	}*/
 	
 
 }
